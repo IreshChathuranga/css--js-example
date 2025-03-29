ba3036db27301430ef9d30bc840ad4b127224658
IreshChathuranga <ireshchathuranga63309.com>
2025-03-29 15:41:07 +0530

    add js

--- .idea/script.js
+++ .idea/script.js
@@ -1,1 +1,11 @@
-alert("Hello Iresh")
+var h1_element = document.getElementsByTagName("h1");
+console.log(h1_element[0]);
+console.log(h1_element[1]);
+console.log(h1_element[0].innerText);
+console.log(h1_element[0].innerText="Hello world");
+
+h1_element[0].innerHTML="<h6>Hello IJSE</h6>";
+
+
+
+
